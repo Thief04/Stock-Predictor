d722b23ede87cb80f8b343f830f8a8a3db3d7074
Thief04 <rohanjwessels@gmail.com>
2023-10-21 07:33:38 -0400

    Test2

--- Stonks.py
+++ Stonks.py
@@ -1,1 +1,11 @@
-print("test")
+from tkinter import *
+
+def create_window():
+    new_window = Tk()
+
+window = Tk()
+
+Button(window,text="Ligma", command=create_window).pack()
+
+
+window.mainloop()
