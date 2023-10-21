9de80aed65ef737377528f655bbc9676d2f139bd
Thief04 <rohanjwessels@gmail.com>
2023-10-21 08:14:12 -0400

    Basic Windows

--- Stonks.py
+++ Stonks.py
@@ -1,11 +1,24 @@
 from tkinter import *
+from tkinter import simpledialog
 
 def create_window():
     new_window = Tk()
+    two_window = Tk()
+    
+    Button(new_window,text="create new window").pack()
+    Button(two_window,text="create new window").pack()
 
-window = Tk()
+    new_window.mainloop()
+    two_window.mainloop()
 
-Button(window,text="Ligma", command=create_window).pack()
+# Display the input dialog box
+user_input = simpledialog.askstring("Input", "Company Ticker:")
 
 
-window.mainloop()
+
+# Check if the user clicked "OK" and entered a value
+if user_input is not None and user_input.strip() != "":
+    create_window()
+else:
+    print("User cancelled the input or entered nothing.")
+
